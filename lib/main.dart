20cfe898c4faba6e5e873971abe6009709b97495
YJSilence <justin82027@gmail.com>
2022-10-30 08:40:16 +0800

    myfirstapp_0921

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  // 建立appTitle和hiFlutter物件
+  var appTitle = Text('我的第一個Flutter App'),
+      hiFlutter = Text(
+        'Hi, Flutter.',
+        style: TextStyle(fontSize: 50),
+      );
+
+  // 建立appBody物件
+  var appBody = Center(
+    child: hiFlutter,
+  );
+
+  //建立appBar物件
+  var appBar = AppBar(
+    title: appTitle,
+  );
+
+  //建立app物件
+  var app = MaterialApp(
+    home: Scaffold(
+      appBar: appBar,
+      body: appBody,
+    ),
+  );
+
+  runApp(app);
 }
 
 class MyApp extends StatelessWidget {
